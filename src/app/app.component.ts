aa9aa5abfad29e564f17239c20fbfb9e8bcc2ecf
vladiko <koganvladimir@yahoo.com>
2017-08-16 21:46:50 +0300

    cleaning app component and adding title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app vladi 2';
+  title = 'Добро пожаловать на встречу выпускников!!!';
 }
