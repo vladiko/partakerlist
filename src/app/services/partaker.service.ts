89a64d7bbd778d2196cb65b47638cb949314ca2b
vladiko <koganvladimir@yahoo.com>
2017-08-21 18:55:39 +0300

    add participants to existing array insted of changing array

--- src/app/services/partaker.service.ts
+++ src/app/services/partaker.service.ts
@@ -5,23 +5,18 @@ import { CommunicationService } from './communication.service';
 
 @Injectable()
 export class PartakerService {
-  public partakers: Partaker[];
+  public partakers: Partaker[] = [];
 
   constructor(private communicationService: CommunicationService) {
     this.communicationService.getPartakerList().then((s) => {
-      this.partakers = s;
+      this.fillArray(this.partakers, s);
     });
-    // this.partakers = [
-    //   new Partaker('Vova', 'Kogan'),
-    //   new Partaker('Erden', 'Magzumov'),
-    //   new Partaker('Tania', 'Borisova')
-    // ];
   }
 
   addPartaker(partaker: Partaker) {
     this.communicationService.addNewPartaker(partaker).then(() => {
       this.communicationService.getPartakerList().then((s) => {
-        this.partakers = s;
+        this.fillArray(this.partakers, s);
       });
     });
   }
@@ -29,8 +24,17 @@ export class PartakerService {
   removePartaker(partaker: Partaker) {
     this.communicationService.removePartaker(partaker).then(() => {
       this.communicationService.getPartakerList().then((s) => {
-        this.partakers = s;
+        this.fillArray(this.partakers, s);
       });
     });
+  }
+
+  private fillArray(a1: Partaker[], a2: Partaker[]) {
+    this.partakers.length = 0;
+    if (a2) {
+      a2.forEach((p) => {
+        a1.push(p);
+      });
+    }
   }
 }
